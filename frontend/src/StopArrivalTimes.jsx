de14afc2fa11043c7bd47ebff96e6dc70bdf7363
Ryan Peden <ryan04@gmail.com>
2016-08-21 17:37:19 -0400

    more updates to arrivals

--- frontend/src/StopArrivalTimes.jsx
+++ frontend/src/StopArrivalTimes.jsx
@@ -4,21 +4,49 @@ import RaisedButton from 'material-ui/RaisedButton';
 import SelectField from 'material-ui/SelectField';
 import {List, ListItem} from 'material-ui/List';
 import MenuItem from 'material-ui/MenuItem';
-import HeightResizingComponent  from './HeightResizingComponent.jsx';
+import {Table, TableBody, TableHeader
+        ,TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import HeightResizingComponent from './HeightResizingComponent.jsx';
 import $ from 'jquery';
 import _ from 'lodash';
 import socket from './SocketIO.js';
 import moment from 'moment-timezone';
 
-const timeToLocalDate = function(timeString) {
+//separate these out into own module
+const timeToLocalDate = (timeString, delay=0) => {
     const timeSplit = timeString.split(':');
+
     let tz = moment.tz('America/Toronto').hours(timeSplit[0])
-                                         .minutes(timeSplit[1]);
+                                         .minutes(timeSplit[1])
+                                         .add(delay, 'seconds');
+
     if(timeSplit.length > 2) {
         tz.seconds(timeSplit[2]);
     }
 
     return tz;
+}
+
+const pluralize = (count, singluar, plural) => {
+    if(count == 1) {
+        return singluar;
+    }
+    return plural;
+}
+
+NOTE TO SELF: switch code below to use this method next
+const toMinutesFromNow = (futureDate) => {
+    const currentTime = moment.tz('America/Toronto');
+    const timeDiffSeconds = Math.floor(departureTime.diff(currentTime) / 1000);
+    const timeDiffHours   = Math.floor(timeDiffSeconds / 3600);
+    //floor because we already account for extra minutes
+    //probably ok to just round here and avoid seconds check elsewhere
+    //but write tests to verify
+    const timeDiffMinutes = Math.floor(timeDiffSeconds / 60);
+
+    const diffString = `${timeDiffHours > 0 ? `${timeDiffHours} hours, ` : ''}` +
+                       `${timeDiffMinutes} minutes`;
+    return diffString;
 }
 
 class StopArrivalTimes extends HeightResizingComponent {
@@ -46,7 +74,6 @@ class StopArrivalTimes extends HeightResizingComponent {
     let stopNum = this.state.stopNumber;
 
     const setState = ::this.setState;
-    const updateArrivals = ::this.updateArrivalTimes;
     const getState = ::this.getState;
 
     const getBufferSize = (secondsToDeparture) => {
@@ -74,18 +101,24 @@ class StopArrivalTimes extends HeightResizingComponent {
 
             const departureSplit = trip.scheduledString.split(':');
 
-            const departureTime = moment.tz('America/Toronto')
-                                      .hour(departureSplit[0])
-                                      .minute(departureSplit[1])
-                                      .add(arrival.delay || 0, 'seconds')
-
+            const departureTime = timeToLocalDate(trip.scheduledString);
             const updatedSeconds = departureTime.second();
 
             if(updatedSeconds > 30) {
                 departureTime.add(1, 'minute');
             }
 
-            trip["departureTime"] = departureTime.format('hh:mm a');
+            const timeDiffSeconds = Math.floor(departureTime.diff(currentTime) / 1000);
+            const timeDiffHours   = Math.floor(timeDiffSeconds / 3600);
+            //floor because we already account for extra minutes
+            //probably ok to just round here and avoid seconds check elsewhere
+            //but write tests to verify
+            const timeDiffMinutes = Math.floor(timeDiffSeconds / 60);
+
+            const diffString = `${timeDiffHours > 0 ? `${timeDiffHours} hours, ` : ''}` +
+                               `${timeDiffMinutes} minutes`;
+
+            trip["departureTime"] = diffString;//departureTime.format('h:mm a');
 
             if(!trip.hasOwnProperty("delays")) {
                 trip["delays"] = [];
@@ -105,7 +138,6 @@ class StopArrivalTimes extends HeightResizingComponent {
     }
 
     $.get(this.arrivalsUrl, function(data){
-
         const trips = JSON.parse(data) || [];
 
         var filteredTrips = _.chain(trips)
@@ -136,59 +168,80 @@ class StopArrivalTimes extends HeightResizingComponent {
     socket.emit('stop-unsubscribe', this.props.params.stopId);
   }
 
-  updateArrivalTimes() {
-
-  }
-
   createArrivalsList() {
     return this.state.trips.map((trip) => {
         let text = "Scheduled";
         if (trip.delays) {
             const bufferedDelay = _.mean(trip.delays);
-            const onTime = bufferedDelay <= 30;
-            const early = bufferedDelay <= -30;
+            const onTime = bufferedDelay <= 29;
+            const early = bufferedDelay <= -29;
 
             if (onTime) {
                 text = "On Time"
             } else if (early) {
-                let minutes = Math.floor(bufferedDelay / 60);
-                text = `${minutes} minutes early`;
+                let delay = Math.round(bufferedDelay / -60);
+                text = `${minutes} ${pluralize(delay,'minute','minutes')} early`;
             } else {
-                let minutes = Math.floor(bufferedDelay / 60);
-                text = `${minutes} minutes late`;
+                let delay = Math.round(bufferedDelay / 60);
+                text = `${minutes} ${pluralize(delay,'minute','minutes')} late`;
             }
         }
 
-        return <ListItem key={trip.tripId}
-                    primaryText={`${trip.departureTime}`}
-                    secondaryText={text} />
+        const rowStyle = {
+            fontSize: '16px'
+        }
+
+        const column = (text) => <TableRowColumn style={rowStyle}>{text}</TableRowColumn>;
+
+        return (
+                <TableRow>
+                    {column(trip.departureTime)}
+                    {column(text)}
+                </TableRow>
+        );
     });
   }
 
   render() {
     const divStyle = {
-      width: '300px',
-      height: this.state.height,
-      maxHeight: this.state.height,
-      boxShadow: '5px 0px 11px -7px rgba(0,0,0,0.50)',
-      zIndex: 10,
-      backgroundColor: 'white',
-      color: 'black',
-      overflow: 'auto'
+        width: '300px',
+        height: this.state.height,
+        maxHeight: this.state.height,
+        boxShadow: '5px 0px 11px -7px rgba(0,0,0,0.50)',
+        zIndex: 10,
+        backgroundColor: 'white',
+        color: 'black',
+        overflow: 'auto'
+    }
+
+    const tableHeaderStyle = {
+        fontSize: '19px'
     }
 
     const header = "Arrivals for Route " + this.props.params.routeId
-                                        + " at Stop "
-                                        + this.props.params.stopId;
+                                         + " at Stop "
+                                         + this.props.params.stopId;
 
     return (
-    <div style={divStyle}>
-      <h3 style={{width: '100%', textAlign:'center'}}>{header}</h3>
-      <List>
-        {this.createArrivalsList()}
-      </List>
-
-    </div>);
+        <div style={divStyle}>
+          <h3 style={{width: '100%', textAlign:'center'}}>{header}</h3>
+          <Table>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn style={tableHeaderStyle}>
+                    Arriving In
+                </TableHeaderColumn>
+                <TableHeaderColumn style={tableHeaderStyle}>
+                    Status
+                </TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+                {this.createArrivalsList()}
+            </TableBody>
+          </Table>
+        </div>
+    );
   }
 }
 
